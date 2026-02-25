8342372aa931fd2faa7f33d2e143982e542440e4
RichardNava <richard_nava@hotmail.com>
2026-02-25 10:40:20 +0100

    mejora buscador sm

--- frontend/src/app/shared/header/header.ts
+++ frontend/src/app/shared/header/header.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink, Router } from '@angular/router';
 import { AuthService } from '../../services/auth';
+import { BuildService } from '../../services/build';
 import { User } from '../../models/user.model';
 
 @Component({
@@ -12,9 +13,11 @@ import { User } from '../../models/user.model';
 export class Header implements OnInit {
   isMenuOpen = false;
   currentUser: User | null = null;
+  searchTerm = '';
 
   constructor(
     private authService: AuthService,
+    private buildService: BuildService,
     private router: Router
   ) {}
 
@@ -22,6 +25,13 @@ export class Header implements OnInit {
     this.authService.currentUser$.subscribe(user => {
       this.currentUser = user;
     });
+  }
+
+  onSearch(): void {
+    this.buildService.setSearchTerm(this.searchTerm);
+    if (this.router.url !== '/home') {
+      this.router.navigate(['/home']);
+    }
   }
 
   toggleMenu(): void {
